82280035e8cd2ff8baec349565073ad145109df5
KyleXu <kyle_x@foxmail.com>
2020-09-15 20:32:06 +0800

    [XTH] using mockito(doReturn) to build test for GradeService

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,40 +1,36 @@
 package stub;
 
 
+import javafx.beans.binding.When;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.xml.transform.Result;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
-    private class mockGradeService extends GradeService{
+    @Mock
+    private GradeSystem mockGradeSystem;
 
-        public mockGradeService(GradeSystem gradeSystem) {
-            super(gradeSystem);
-        }
-
-        @Override
-        public double calculateAverageGrades(long studentId) {
-            List<Double> doubles = Arrays.asList(80.0, 90.0, 100.0);
-
-            double averageGrade = doubles.stream()
-                    .mapToDouble((grade) -> grade)
-                    .average()
-                    .orElse(0);
-
-            return averageGrade;
-        }
-    }
+    @InjectMocks
+    private GradeService gradeService;
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        GradeSystem gradeSystem = new GradeSystem();
-        GradeService gradeService = new mockGradeService(gradeSystem);
+        doReturn(Arrays.asList(80.0, 90.0, 100.0)).when(mockGradeSystem).gradesFor(1);
 
         Assertions.assertEquals(90.0, gradeService.calculateAverageGrades(1));
     }
